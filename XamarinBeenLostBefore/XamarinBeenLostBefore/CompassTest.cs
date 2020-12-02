afd621657ec1b555fc15c5dab43afb2917bffb81
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-12-01 20:36:43 -0600

    created compasstest class

---
+++ XamarinBeenLostBefore/XamarinBeenLostBefore/CompassTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Essentials;
+public class CompassTest
+{
+    // Set speed delay for monitoring changes.
+    SensorSpeed speed = SensorSpeed.UI;
+
+    public CompassTest()
+    {
+        // Register for reading changes, be sure to unsubscribe when finished
+        Compass.ReadingChanged += Compass_ReadingChanged;
+    }
+
+    void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+    {
+        var data = e.Reading;
+        Console.WriteLine($"Reading: {data.HeadingMagneticNorth} degrees");
+        // Process Heading Magnetic North
+    }
+
+    public void ToggleCompass()
+    {
+        try
+        {
+            if (Compass.IsMonitoring)
+                Compass.Stop();
+            else
+                Compass.Start(speed, applyLowPassFilter: true);
+        }
+        catch (FeatureNotSupportedException fnsEx)
+        {
+            // Feature not supported on device
+        }
+        catch (Exception ex)
+        {
+            // Some other exception has occurred
+        }
+    }
+}
